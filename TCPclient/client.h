d22ebd9afe01411611086fb754f0be3b6a780335
Zahra <zahra.parsaeian1996@gmail.com>
2016-06-20 17:53:58 +0430

    file

--- TCPclient/client.h
+++ TCPclient/client.h
@@ -2,12 +2,25 @@
 #define CLIENT_H
 
 #include <QWidget>
+#include<QTcpSocket>
+#include<Qstring>
 
 class Client : public QWidget
 {
     Q_OBJECT
 public:
     explicit Client(QWidget *parent = 0);
+
+private slots:
+    void connectedToServer();
+    void disconnectByServer();
+
+
+private:
+    QTcpSocket *m_socket;
+    QString m_user;
+    QString m_receivedData;
+
 
 signals:
 
