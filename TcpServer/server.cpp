2e42b5788114643e7d43f49053d9021a0fb4b7fd
Zahra <zahra.parsaeian1996@gmail.com>
2016-06-20 10:37:14 +0430

    Adding removing connection's codes

--- TcpServer/server.cpp
+++ TcpServer/server.cpp
@@ -1,7 +1,37 @@
 #include "server.h"
+#include<QDebug>
 
-Server::Server(QWidget *parent) : QWidget(parent)
-{
+Server::Server(QWidget *parent) : QWidget(parent){
+    m_server=new QTcpServer(this);
+
+    if (!m_server->listen(QHostAddress::LocalHost, 52693)) {
+        qDebug()<<"Connection failed";
+        return;
+    }
+    connect(m_server, SIGNAL(newConnection()), this, SLOT(newConnection()));
+
+}
+
+void Server::newConnection(){
+    while (m_server->hasPendingConnections()) {
+          QTcpSocket *con = m_server->nextPendingConnection();
+          m_clients << con;
+          connect(con, SIGNAL(disconnected()), this, SLOT(removeConnection()));
+}
+
+}
+
+void Server::removeConnection(){
+    if (QTcpSocket *con = qobject_cast<QTcpSocket*>(sender())){
+        /*ui->log->insertPlainText(QString("* Connection removed: %1, port %2\n")
+                                 .arg(con->peerAddress().toString())
+                                 .arg(QString::number(con->peerPort())));*/
+        m_clients.removeOne(con);
+        con->deleteLater();
+        //ui->disconnectClients->setEnabled(!m_clients.isEmpty());
+        qDebug()<<"Connection removed";
+    }
+
 
 }
 
--- TcpServer/server.h
+++ TcpServer/server.h
@@ -2,12 +2,29 @@
 #define SERVER_H
 
 #include <QWidget>
+#include<QTcpServer>
+#include<QTcpSocket>
+#include<QList>
+
 
 class Server : public QWidget
 {
     Q_OBJECT
 public:
     explicit Server(QWidget *parent = 0);
+
+private slots:
+    void newConnection();
+    void removeConnection();
+    //void newMessage();
+
+    //void on_disconnectClients_clicked();
+
+private:
+    //Ui::TcpServer *ui;
+    QTcpServer *m_server;
+    QList<QTcpSocket*> m_clients;
+    //QHash<QTcpSocket*, QString> m_receivedData;
 
 signals:
 
