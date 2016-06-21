1f5964a5595830fff110fc050f4a9f941553680a
Zahra <zahra.parsaeian1996@gmail.com>
2016-06-21 17:41:51 +0430

    change main

--- Ser/server.h
+++ Ser/server.h
@@ -2,12 +2,24 @@
 #define SERVER_H
 
 #include <QWidget>
+#include<QTcpServer>
+#include<QTcpSocket>
 
 class Server : public QWidget
 {
     Q_OBJECT
 public:
     explicit Server(QWidget *parent = 0);
+
+private slots:
+    void newConnection();
+    void removeConnection();
+    void newMessage();
+
+private:
+    QTcpServer *m_server;
+    QList<QTcpSocket*> m_clients;
+    QHash<QTcpSocket*, QString> m_receivedData;
 
 signals:
 
--- build-Ser-Desktop_Qt_5_5_1_MSVC2013_64bit-Debug/debug/moc_server.cpp
+++ build-Ser-Desktop_Qt_5_5_1_MSVC2013_64bit-Debug/debug/moc_server.cpp
@@ -6,7 +6,7 @@
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../../Server1/server.h"
+#include "../../Ser/server.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
