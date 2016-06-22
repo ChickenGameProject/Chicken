af81b26401f12658686bfd9228c1abea245a06b3
tahereh talaei <taheretalaei@gmail.com>
2016-06-22 16:37:06 +0430

    add life

---
+++ game/life.h
@@ -0,0 +1,18 @@
+#ifndef LIFE_H
+#define LIFE_H
+
+#include <QGraphicsPixmapItem>
+
+class Life : public QGraphicsPixmapItem
+{
+    Q_OBJECT
+public:
+    explicit Life(QObject *parent = 0);
+
+signals:
+
+public slots:
+
+};
+
+#endif // LIFE_H
